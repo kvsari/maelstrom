f624af683ffeb4d88801d1845a2915fd4e70830f
Stephan Luther <kvsari@gmail.com>
2018-10-03 23:21:07 +0900

    Flesh out the API a little

---
+++ README.md
@@ -0,0 +1,3 @@
+Malestrom
+===
+This crate is inspired by reading about the [LMAX disruptor](https://lmax-exchange.github.io/disruptor/) pattern and also the technical interest in creating a multiple producer multiple consumer channel in rust. This project is a more serious attempt at a MPMC than my simple quick-n-dirty [channel-broadcast](https://github.com/kvsari/channel-broadcast) project.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+//! # Maelstrom
+//! An attempt at a multiple producer multiple consumer channel in rust.
+
+/// # Inlet
+/// It is the transmission (tx) pair in the channel.
+#[derive(Clone)]
+pub struct Inlet;
+
+/// # Outlet
+/// It is the receiver (rx) pair in the channel.
+#[derive(Clone)]
+pub struct Outlet;
+
+/// Create a maelstrom channel. The transmitter `Inlet` and the receiver `Outlet` 
+pub fn maelstrom() -> (Inlet, Outlet) {
+    (Inlet, Outlet)
 }
